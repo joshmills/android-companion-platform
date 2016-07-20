01ae742446935b3868d6a320ce64ffa7d340d8a1
Josh Mills <josh.mills4@gmail.com>
2016-07-20 12:45:07 +0100

    Removed unused variable.

--- xapk-builder/src/co/jmills/xapk/ZipHandler.java
+++ xapk-builder/src/co/jmills/xapk/ZipHandler.java
@@ -4,10 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
 
 /**
  * 
@@ -15,7 +13,6 @@ import java.util.zip.ZipOutputStream;
  */
 public class ZipHandler {
  
-	private List<String> fileList;
 	private String mInputZipFile;
 	private String mOutputFolder;
 	
